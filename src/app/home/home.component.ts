ac24adb4e42cc13916385b5a9e51241725e68820
mohammadjh <mohammadjh@callnsolution.com>
2018-02-22 19:20:00 +0600

    Modify home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,6 +7,19 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None
 })
 export class HomeComponent implements OnInit {
+  title = 'Jingle Bell';
+  answer: string = '';
+  answerDisplay: string = '';
+  showSpinner: boolean = false;
+
+  showAnswer() {
+    this.showSpinner = true;
+
+    setTimeout(()=> {
+      this.answerDisplay = this.answer;
+      this.showSpinner = false;
+    }, 2000);
+  }
 
   constructor() { }
 
